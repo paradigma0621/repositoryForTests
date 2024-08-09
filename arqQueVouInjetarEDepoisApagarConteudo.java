ede37f7904b67cfb3165cda23ed54fa49eed30e1
paradigma0621@gmail.com <paradigma0621@gmail.com>
2024-08-09 07:26:03 -0300

    (branchDaQualInjetareiEApagarei) arqQueVouInjetarEDepoisApagarConteudo criado

---
+++ arqQueVouInjetarEDepoisApagarConteudo.java
@@ -0,0 +1,18 @@
+package x.y.z;
+
+{
+	código 	
+	código 	
+	código 	
+	código 	
+	
+	// Comentários 
+	// Comentários
+	// Comentários	
+	// Comentários	
+
+	mais código 	
+	mais código 	
+	mais código 	
+		
+}
